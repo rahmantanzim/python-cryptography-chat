ae22035ab6d426526b3c94fb352a9558564c5e3e
Tanzim Rahman <tanzimr@mun.ca>
2025-11-21 19:13:14 -0330

    created teh function: gen_and_save_keys

--- src/security/key_manager.py
+++ src/security/key_manager.py
@@ -16,5 +16,34 @@ DEFAULT_PRIVATE_KEY_PATH = os.path.join(DEFAULT_KEYS_DIR, "private_key.pem")
 DEFAULT_PUBLIC_KEY_PATH = os.path.join(DEFAULT_KEYS_DIR, "public_key.pem")
 
 def _ensure_keys_dir_exists() -> None:
-    """Create the keys/ directory if it does not exist."""
+    """Create the 'keys/' directory if not exist."""
     os.makedirs(DEFAULT_KEYS_DIR, exist_ok=True)
+    
+def gen_and_save_keys(
+    private_key_path: str = DEFAULT_PRIVATE_KEY_PATH,
+    public_key_path: str = DEFAULT_PUBLIC_KEY_PATH,
+    key_size: int = 2048 #2048 is secured and fast at the same time
+    ) -> Tuple[str,str]: #this function will return a tuple with 2 strings which are actually the path of 2 files
+        _ensure_keys_dir_exists()
+        
+        private_key = rsa.generate_private_key(
+            public_exponent= 65537,
+            key_size=key_size
+        ) # this generates a private key object and stores in the private_key variable
+        public_key = private_key.public_key()
+        
+        #serialization: Private key
+        with open(private_key_path,'wb') as f:
+            f.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM, #key will be encoded in PEM format
+                format=serialization.PrivateFormat.PKCS8, #modern recommended format for private keys.
+                encryption_algorithm= serialization.NoEncryption(), #private key file will be plain-text PEM without a password
+            ))
+        with open(public_key_path,'wb') as f:
+            f.write(
+                public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                )
+            )
+        return private_key_path, public_key_path
